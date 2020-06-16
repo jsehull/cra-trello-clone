63029ae5f7521554fbed42fc9b075dc930842e9f
Jesse Hull <jse.hull@gmail.com>
2020-06-16 15:21:35 -0600

    add basic layout for columns/cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import {AppContainer} from './styles'
+import {Column} from './Column'
+import {Card} from './Card'
 
 const App = () => {
   return (
     <AppContainer>
-      test
+      <Column text="To Do">
+        <Card text="state??" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="adding more components" />
+      </Column>
+      <Column text="Done">
+        <Card text="Started new App" />
+      </Column>
     </AppContainer>
   )
 }
