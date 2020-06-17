8eaffcc11d0333937bd03e626468e768e4d05f2f
Jesse Hull <jse.hull@gmail.com>
2020-06-17 10:13:06 -0600

    add focus to btn inputs

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,19 +1,21 @@
 import React, {useState} from 'react'
 import { NewItemFormContainer, NewItemButton, NewItemInput } from './styles'
+import {useFocus} from './utils/useFocus'
 
 
 interface NewItemFormProps {
   onAdd(text: string): void
 }
 
-export const NewItemForm = (props: NewItemFormProps) => {
+export const NewItemForm = ({onAdd}: NewItemFormProps) => {
   const [text, setText] = useState('')
-  const {onAdd} = props
+  const inputRef = useFocus()
 
   return (
     <NewItemFormContainer>
       <NewItemInput
         value={text}
+        ref={inputRef}
         onChange={e => setText(e.target.value)}
       />
       <NewItemButton onClick={() => onAdd(text)}>
